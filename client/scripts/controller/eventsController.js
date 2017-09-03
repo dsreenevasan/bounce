840ec25bac6318ec998a12b148b96195deaa7134
CTF Tech Team - Frontend <frontend@kurukshetra.org.in>
2017-09-04 00:09:17 +0530

    just a try with bounce js

--- client/scripts/controller/eventsController.js
+++ client/scripts/controller/eventsController.js
@@ -9,21 +9,30 @@
         ctrl.title = "Home Page";
         ctrl.animated = false;
         ctrl.bigDiv = false;
-        
+        ctrl.flag = false;
         ctrl.animate = function () {
+            console.log("1");
             if(!ctrl.bigDiv){
                 ctrl.animated = !ctrl.animated;
+                ctrl.flag = !ctrl.flag;
 
                 $timeout(function() {
                     ctrl.bigDiv = !ctrl.bigDiv;
                     ctrl.animated = !ctrl.animated;
                 }, 3000);
             } else{
+
+            }
+        };
+
+        ctrl.reverseAnimation = function () {
+            console.log("2");
+            if(ctrl.bigDiv){
                 ctrl.reverseAnimated = !ctrl.reverseAnimated;
                 ctrl.bigDiv = !ctrl.bigDiv;
 
                 $timeout(function() {
-
+                    ctrl.flag = !ctrl.flag;
                     ctrl.reverseAnimated = !ctrl.reverseAnimated;
                 }, 3000);
             }
